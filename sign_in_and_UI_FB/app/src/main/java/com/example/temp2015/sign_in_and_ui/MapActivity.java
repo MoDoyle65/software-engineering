b41c3ce24e1462db049b59f15d3caf680a2b2ea7
03431568 <ronan.mc-cormack.1@ucdconnect.ie>
2016-12-19 15:25:18 +0000

    Current location on map - not working - do not rebase on

--- sign_in_and_UI_FB/app/src/main/java/com/example/temp2015/sign_in_and_ui/MapActivity.java
+++ sign_in_and_UI_FB/app/src/main/java/com/example/temp2015/sign_in_and_ui/MapActivity.java
@@ -1,29 +1,70 @@
 package com.example.temp2015.sign_in_and_ui;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity implements  OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+    private static final String TAG = "Hello";
+    private GoogleMap googleMap;
+    private CameraPosition mCameraPosition;
+
+    // The entry point to Google Play services, used by the Places API and Fused Location Provider.
+    private GoogleApiClient mGoogleApiClientMap;
+    // A request object to store parameters for requests to the FusedLocationProviderApi.
+    private LocationRequest mLocationRequest;
+    // The desired interval for location updates. Inexact. Updates may be more or less frequent.
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+    // The fastest rate for active location updates. Exact. Updates will never be more frequent
+    // than this value.
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+    // A default location (dublin) and default zoom to use when location permission is
+    // not granted.
+    private final LatLng mDefaultLocation = new LatLng(53.350140, -6.26615);
+    private static final int DEFAULT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted;
+
+    // The geographical location where the device is currently located.
+    private Location mCurrentLocation;
+
+    // Keys for storing activity state.
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
     private FirebaseConnection mFirebaseConnection1, mFirebaseConnection2;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mFirebaseRef;
     private DatabaseReference pushRef;
+    private LatLng myLocation;
     private getPinHeaderCallback pincb = new getPinHeaderCallback();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         Bundle extras = getIntent().getExtras();
@@ -35,33 +76,188 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mFirebaseConnection2 = new FirebaseConnection(mFirebaseRef,mFirebaseRef.child(uid));
         // Get the SupportMapFragment and request notification
         // when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        // Retrieve location and camera position from saved instance state.
+        if (savedInstanceState != null) {
+            mCurrentLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+
+        // Build the Play services client for use by the Fused Location Provider and the Places API.
+        Log.d("Eror check", "Errock");
+        mGoogleApiClientMap = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(LocationServices.API)
+                .build();
+
+
+        if (mGoogleApiClientMap.isConnected()){
+        Log.d("isConnected","Connected");}
+
     }
+
 
     /**
      * Manipulates the map when it's available.
      * The API invokes this callback when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user receives a prompt to install
      * Play services inside the SupportMapFragment. The API invokes this method after the user has
      * installed Google Play services and returned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
 
+        updateLocationUI();
+        System.out.println("Hello");
         mFirebaseConnection1.getUserPinHeaders(pincb, googleMap);
         mFirebaseConnection2.getFriendsPins(pincb, googleMap);
+        //googleMap.getMyLocation()
+        //googleMap.setLatLngBoundsForCameraTarget();
+        if (mCameraPosition != null) {
+            this.googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(mCameraPosition));
+        } else if (mCurrentLocation != null) {
+            this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(mCurrentLocation.getLatitude(),
+                            mCurrentLocation.getLongitude()), DEFAULT_ZOOM));
+        } else {
+            Log.d(TAG, "Current location is null. Using defaults.");
+            this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+            this.googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+    }
 
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+
+        /*
+         * Sets the desired interval for active location updates. This interval is
+         * inexact. You may not receive updates at all if no location sources are available, or
+         * you may receive them slower than requested. You may also receive updates faster than
+         * requested if other applications are requesting location at a faster interval.
+         */
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        /*
+         * Sets the fastest rate for active location updates. This interval is exact, and your
+         * application will never receive updates faster than this value.
+         */
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void updateLocationUI() {
+        if (googleMap == null) {
+            return;
+        }
+
+        if (mLocationPermissionGranted) {
+            googleMap.setMyLocationEnabled(true);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        } else {
+            googleMap.setMyLocationEnabled(false);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+            mCurrentLocation = null;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGoogleApiClientMap.isConnected()) {
+            getDeviceLocation();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+        createLocationRequest();
+        getDeviceLocation();
+        // Build the map.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    /**
+     * Stop location updates when the activity is no longer in focus, to reduce battery consumption.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClientMap.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(
+                    mGoogleApiClientMap, this);
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClientMap.connect();
+    }
+    private void getDeviceLocation() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         * Also request regular updates about the device location.
+         */
+        if (mLocationPermissionGranted) {
+            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClientMap);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClientMap,
+                    mLocationRequest, this);
+        }
+    }
+
+    /**
+     * Handles the result of the request for location permissions.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+    }
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.d(TAG, "Play services connection suspended");
+    }
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult result) {
+        // Refer to the reference doc for ConnectionResult to see what error codes might
+        // be returned in onConnectionFailed.
+        Log.d(TAG, "Play services connection failed: ConnectionResult.getErrorCode() = "
+                + result.getErrorCode());
     }
 }
-
-
-
-
 
 
